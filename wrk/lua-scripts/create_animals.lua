16150a1a0f3738bbc062da1757811b9f7a997c15
Marco Bungart <marco.bungart@googlemail.com>
2023-04-11 16:05:12 +0200

    nice formatting for wrk tests

--- wrk/lua-scripts/create_animals.lua
+++ wrk/lua-scripts/create_animals.lua
@@ -7,21 +7,34 @@ request = function()
    return wrk.format("POST", url_path, headers, Animal:new():json())
 end
 
+local microseconds_per_millisecond = 1000.0;
+
 done = function(summary, latency, _)
-    io.write("-------------- CREATE ANIMALS -----------------\n")
+    io.write("------------ CREATE ANIMALS ------------\n")
     io.write(string.format("total number of requests: %d\n", summary.requests));
+    local status_symbol = summary.errors.status == 0 and "✅" or "❌"
     io.write(string.format(
-            "Non-2xx responses: %d (%.2f%%)\n",
+            "Non-2xx responses: %d (%.2f%%) %s\n",
             summary.errors.status,
-            summary.errors.status / summary.requests * 100))
-    io.write("-------------- Latency ------------------------\n")
-    io.write(string.format("stdev: %f\n", latency.stdev))
-    io.write(string.format("min  : %d\n", latency.min))
-    io.write(string.format("max  : %d\n", latency.max))
+            summary.errors.status / summary.requests * 100,
+            status_symbol))
+    io.write("--------------- Latency ----------------\n")
+    io.write(string.format(
+            "     stdev : %7d µs = %8.3f ms\n",
+            latency.stdev,
+            latency.stdev / microseconds_per_millisecond))
+    io.write(string.format(
+            "       min : %7d µs = %8.3f ms\n",
+            latency.min,
+            latency.min / microseconds_per_millisecond))
+    io.write(string.format(
+            "       max : %7d µs = %8.3f ms\n",
+            latency.max,
+            latency.max / microseconds_per_millisecond))
     io.write("Percentiles:\n")
     for _, p in pairs({ 0.001, 0.01, 0.1, 1, 10, 50, 90, 99, 99.9, 99.99, 99.999 }) do
         n = latency:percentile(p)
-        io.write(string.format("  %6.3f%% -> %d\n", p, n))
+        io.write(string.format("  %7.3f%% : %7d µs = %8.3f ms\n", p, n, n / microseconds_per_millisecond))
     end
-	io.write("-----------------------------------------------\n\n")
+	io.write("----------------------------------------\n\n")
 end
