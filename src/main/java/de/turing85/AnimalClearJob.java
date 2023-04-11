a25de3e57f1fbf4f74d09dc20d3f858a3d3b6fc5
Marco Bungart <marco.bungart@googlemail.com>
2023-04-12 01:57:07 +0200

    Add cron to cleanup database

---
+++ src/main/java/de/turing85/AnimalClearJob.java
@@ -0,0 +1,25 @@
+package de.turing85;
+
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.quarkus.scheduler.Scheduled;
+import io.smallrye.mutiny.Uni;
+import javax.enterprise.context.ApplicationScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
+public class AnimalClearJob {
+  private final AnimalRepository repository;
+
+  @Scheduled(cron = "${db.cleanup.cron}")
+  @ReactiveTransactional
+  Uni<Void> clearAnimals() {
+    return Uni.createFrom().nullItem()
+        .onItem().invoke(() -> log.info("clearing animals"))
+        .flatMap(ignored -> repository.deleteAll())
+        .replaceWithVoid()
+        .onItem().invoke(() -> log.info("animals cleared"));
+  }
+}
